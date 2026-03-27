177858ff6f0be27e59391d61a3728d2211f4fc99
Aayush kumar <aayush.rtk2024@gmail.com>
2026-03-27 23:27:15 +0530

    Delete SOS.java

--- SOS.java
+++
@@ -1,8 +0,0 @@
-public class SOS {
-
-    public static void sendAlert(String name) {
-        System.out.println("\n🚨 SOS ALERT ACTIVATED 🚨");
-        System.out.println("User: " + name);
-        System.out.println("Emergency contacts notified!");
-    }
-}
